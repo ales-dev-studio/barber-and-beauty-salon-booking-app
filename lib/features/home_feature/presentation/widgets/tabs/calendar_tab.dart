6574de55e04cd5939d85a75561003165a8bde4d5
ales <ales.dev.studio@gmail.com>
2025-05-31 20:51:46 +0300

    Initial commit

---
+++ lib/features/home_feature/presentation/widgets/tabs/calendar_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CalendarTab extends StatelessWidget {
+  const CalendarTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('CalendarTab'),
+    );
+  }
+}
