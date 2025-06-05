551a84ad9403045dc3f3997f662b44f3fc306682
ales <ales.dev.studio@gmail.com>
2025-06-05 21:31:31 +0300

    Add a sample banner to the home screen

--- lib/features/home_feature/presentation/widgets/tabs/home_tab.dart
+++ lib/features/home_feature/presentation/widgets/tabs/home_tab.dart
@@ -1,3 +1,5 @@
+import 'package:barber_and_beauty_salon_booking_app/core/gen/assets.gen.dart';
+import 'package:barber_and_beauty_salon_booking_app/core/theme/dimens.dart';
 import 'package:barber_and_beauty_salon_booking_app/core/widgets/app_search_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,15 @@ class HomeTab extends StatelessWidget {
       child: Column(
         children: [
           AppSearchBar(),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(
+              top: Dimens.padding,
+              left: Dimens.largePadding,
+              right: Dimens.largePadding,
+            ),
+            child: Assets.images.banner.image(fit: BoxFit.fitWidth),
+          ),
         ],
       ),
     );
