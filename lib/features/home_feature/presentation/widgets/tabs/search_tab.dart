9117a45c1525e4949a8f58a867b286191e921b21
ales <ales.dev.studio@gmail.com>
2025-06-05 23:55:02 +0300

    Implement the search tab

--- lib/features/home_feature/presentation/widgets/tabs/search_tab.dart
+++ lib/features/home_feature/presentation/widgets/tabs/search_tab.dart
@@ -1,3 +1,8 @@
+import 'package:barber_and_beauty_salon_booking_app/core/theme/dimens.dart';
+import 'package:barber_and_beauty_salon_booking_app/core/widgets/app_search_bar.dart';
+import 'package:barber_and_beauty_salon_booking_app/features/home_feature/presentation/widgets/service_list.dart';
+import 'package:barber_and_beauty_salon_booking_app/features/home_feature/presentation/widgets/service_title_widget.dart';
+import 'package:barber_and_beauty_salon_booking_app/features/home_feature/presentation/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
 
 class SearchTab extends StatelessWidget {
@@ -5,8 +10,24 @@ class SearchTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('SearchTab'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          AppSearchBar(),
+          SizedBox(height: Dimens.largePadding),
+          TopCategories(),
+          SizedBox(height: Dimens.padding),
+          ServiceTitleWidget(title: 'Recent', onPressed: () {}),
+          ServiceList(),
+          SizedBox(height: Dimens.padding),
+          ServiceTitleWidget(title: 'Popular services', onPressed: () {}),
+          ServiceList(),
+          SizedBox(height: Dimens.padding),
+          ServiceTitleWidget(title: 'Nearby beauty salon', onPressed: () {}),
+          ServiceList(),
+          SizedBox(height: Dimens.largePadding),
+        ],
+      ),
     );
   }
 }
